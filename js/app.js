1a28524d99b393f8e7faa4d59a0528e65ad40509
U-BEIJING\aaronliu <aaronliu@AaronLiu-pc.Beijing.Objectiva.local>
2016-02-02 19:33:55 +0800

    Init

---
+++ js/app.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Router from 'react-router';
+import { DefaultRoute, Link, Route, RouteHandler } from 'react-router';
+
+import HelloHandler from './hello.js';
+import Timer from './timer.js';
+import Menu from './menu.js';
+import AddFiles from './addFiles.js';
+
+
+let App = React.createClass({
+  render() {
+    return (
+      <div className="nav">
+        <Link to="app" className="homelink">Home  </Link>
+        <Link to="hello" className="hellolink">  Say Hello</Link>
+        <RouteHandler/>
+        <Timer start={Date.now()} />
+        <Menu items={['Home','About','Contect']} />
+        <AddFiles />
+
+      </div>
+    );
+  }
+});
+
+let routes = (
+  <Route name="app" path="/" handler={App}>
+    <Route name="hello" path="/hello" handler={HelloHandler}/>
+  </Route>
+);
+
+Router.run(routes, function (Handler) {
+  React.render(<Handler />, document.body);
+});
---
+++ js/hello.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+let Hello = React.createClass({
+  render() {
+    return(<div>Hello World!</div>);
+  }
+});
+
+export default Hello;
